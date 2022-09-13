40a6bf12f3bd7ee471fc71b187799543c24d8e80
Aakarsh2000 <saiaakarshp@gmail.com>
2022-09-13 22:28:47 +0530

    Added Feautered Videos column

--- src/components/Column3.js
+++ src/components/Column3.js
@@ -1,10 +1,11 @@
 import React from 'react'
 import '../App.css'
+import Featured_videos from './Column3_components/Featured_videos'
 
 function Column3() {
   return (
     <div className='cb-col cb-col-33'>
-      Featured Videos
+      <Featured_videos/>
     </div>
   )
 }
---
+++ src/components/Column3_components/Featured_videos.js
@@ -0,0 +1,50 @@
+import React from 'react'
+import img1 from '../../images/india-have-picked-the-right-sq.jpg'
+import img2 from '../../images/australia-south-africa-serie.jpg'
+import img3 from '../../images/answered-what-should-be-india.jpg'
+
+function Featured_videos() {
+  return (
+    <div class="cb-col cb-col-100 cb-hm-rt-itm cb-bg-white">
+        <h4 class="cb-mdl-hdr heading">Featured Videos</h4>
+        <div class="cb-col cb-col-100 cb-mid-wrp">
+            <a class="suggested-video-gtm" gtm-label="India have picked the right squad, sorted combination a priority: Harsha Bhogle|57317" title="India have picked the right squad, sorted combination a priority: Harsha Bhogle">
+                <div class="cb-pos-rel"><img class="cb-suggested-vid-img suggested-video-gtm" gtm-label="India have picked the right squad, sorted combination a priority: Harsha Bhogle|57317" alt="India have picked the right squad, sorted combination a priority: Harsha Bhogle" title="India have picked the right squad, sorted combination a priority: Harsha Bhogle" src={img1} height="164" width="290"/>
+                    <div class="cb-cen">
+                        <div class="cb-vid-sm-ply-api">►</div>
+                    </div>
+                </div>
+                <h4 class="text-hvr-underline suggested-video-gtm heading" gtm-label="India have picked the right squad, sorted combination a priority: Harsha Bhogle|57317">India have picked the right squad, sorted combination a priority: Harsha Bhogle</h4>
+            </a>
+            <div class="cb-nws-time">2h ago</div>
+        </div>
+        <div class="cb-col cb-col-100 cb-mid-wrp">
+            <a class="suggested-video-gtm" gtm-label="Australia &amp; South Africa series crucial for Rohit, Rahul &amp; Kohli: Parthiv Patel|57244" title="Australia &amp; South Africa series crucial for Rohit, Rahul &amp; Kohli: Parthiv Patel">
+                <div class="cb-pos-rel"><img class="cb-suggested-vid-img suggested-video-gtm" gtm-label="Australia &amp; South Africa series crucial for Rohit, Rahul &amp; Kohli: Parthiv Patel|57244" alt="Australia &amp; South Africa series crucial for Rohit, Rahul &amp; Kohli: Parthiv Patel" title="Australia &amp; South Africa series crucial for Rohit, Rahul &amp; Kohli: Parthiv Patel" src={img2} height="164" width="290"/>
+                    <div class="cb-cen">
+                        <div class="cb-vid-sm-ply-api">►</div>
+                    </div>
+                </div>
+                <h4 class="text-hvr-underline suggested-video-gtm heading" gtm-label="Australia &amp; South Africa series crucial for Rohit, Rahul &amp; Kohli: Parthiv Patel|57244">Australia &amp; South Africa series crucial for Rohit, Rahul &amp; Kohli: Parthiv Patel</h4>
+            </a>
+            <div class="cb-nws-time">Sep 08 2022</div>
+        </div>
+        <div class="cb-col cb-col-100 cb-mid-wrp">
+            <a target="_self" href="/cricket-videos/57265/answered-what-should-be-indias-bowling-attack-for-t20-world-cup" class="suggested-video-gtm" gtm-label="Answered: What should be India's bowling attack for T20 World Cup?|57265" title="Answered: What should be India's bowling attack for T20 World Cup?">
+                <div class="cb-pos-rel"><img class="cb-suggested-vid-img suggested-video-gtm" gtm-label="Answered: What should be India's bowling attack for T20 World Cup?|57265" alt="Answered: What should be India's bowling attack for T20 World Cup?" title="Answered: What should be India's bowling attack for T20 World Cup?" src={img3} height="164" width="290"/>
+                    <div class="cb-cen">
+                        <div class="cb-vid-sm-ply-api">►</div>
+                    </div>
+                </div>
+                <h4 class="text-hvr-underline suggested-video-gtm heading" gtm-label="Answered: What should be India's bowling attack for T20 World Cup?|57265">Answered: What should be India's bowling attack for T20 World Cup?</h4>
+            </a>
+            <div class="cb-nws-time">Sep 08 2022</div>
+        </div>
+        <div class="cb-col cb-col-100 cb-more-btn-cntnr">
+            <a title="Click to view more videos" class="cb-more-btn" role="button">More Videos</a>
+        </div>
+    </div>
+  )
+}
+
+export default Featured_videos
